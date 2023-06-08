c8cb0f55ba719b044f000a3f78b0c7ceab7067c2
ChhabiNeup <135876683+ChhabiNeup@users.noreply.github.com>
2023-06-08 14:49:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
 except URLError as e:
     streamlit.error()
      
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
         with my_cnx.cursor() as my_cur:
              my_cur.execute("select * from fruit_load_list")   
@@ -57,7 +57,7 @@ if streamlit.button('Get Fruit Load List'):
        #streamlit.stop()
 def insert_row_snowflake(new_fruit):
         with my_cnx.cursor() as my_cur:
-                my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+                my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
                 return "Thanks for adding " + new_fruit
        
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
